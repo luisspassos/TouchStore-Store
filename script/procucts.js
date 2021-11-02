1f32dfa4583e3b97b77e307fa182ee1035e5a68e
Luís Passos <luis.passos013@gmail.com>
2021-11-02 19:10:01 -0300

    add script directory

---
+++ script/procucts.js
@@ -0,0 +1,49 @@
+const products = [
+    {
+        title: 'iPhone 11 Apple (128GB) (PRODUCT)RED tela 6,1" Câmera 12MP iOS',
+        price: 4096.80,
+        img: "https://images-americanas.b2w.io/produtos/01/00/img/1611344/8/1611344831_1SZ.jpg"
+    },
+    {
+        title: 'Smart TV LED 32" Philco PTV32G52S HD e Áudio Dolby Conversor Digital Integrado 2 HDMI 1 USB Wi-Fi com Netflix',
+        price: 1329.99,
+        img: "https://images-americanas.b2w.io/produtos/01/00/img/1322119/4/1322119477_1SZ.jpg"
+    },
+    {
+        title: "Panela De Pressão Panelux Fechamento Externo 4.5L Alumínio",
+        price: 109.99,
+        img: "https://images-americanas.b2w.io/produtos/01/00/img/134281/8/134281848_1SZ.jpg"
+    },
+    {
+        title: 'Monitor LED 24" Samsung LC24f390 1920x1080 Full HD Curvo - Preto',
+        price: 975.57,
+        img: "https://images-americanas.b2w.io/produtos/01/00/img7/01/00/item/133321/7/133321705SZ.jpg"
+    },
+    {
+        title: "Mouse Gamer Logitech G203 LIGHTSYNC RGB Efeito de Ondas de Cores 6 Botões Programáveis e Até 8.000 DPI - Preto",
+        price: 126.95,
+        img: "https://images-americanas.b2w.io/produtos/01/00/img/1720328/9/1720328928_1SZ.jpg"
+    },
+    {
+        title: "Cadeira Gamer Elite Preto - Dazz",
+        price: 799.99,
+        img: "https://images-americanas.b2w.io/produtos/01/00/img/133776/0/133776048_1SZ.jpg"
+    },
+    {
+        title: "Mesa Digitalizadora CTL472 One By Wacom Redwood",
+        price: 287.09,
+        img: "https://images-americanas.b2w.io/produtos/01/00/img/133710/3/133710372_1SZ.jpg"
+    },
+    {
+        title: "Caneta Esferográfica Bic Fashion Desliza 10 Cores 930813 26600",
+        price: 19.99,
+        img: "https://images-americanas.b2w.io/produtos/50862318/imagens/caneta-esferografica-bic-fashion-desliza-10-cores-930813-26600/50862318_1_xlarge.jpg"
+    },
+    {
+        title: "Papel Manteiga para embrulho 30x35cm 1204 500 unidades",
+        price: 119.00,
+        img: "https://images-americanas.b2w.io/produtos/3484218021/imagens/papel-manteiga-para-embrulho-30x35cm-1204-500-unidades/3484218021_1_xlarge.jpg"
+    }
+]
+
+export { products }
