2c57979a75091d94d252553e76d5ed7575e0dada
Luís Passos <luis.passos013@gmail.com>
2023-05-05 20:47:53 -0300

    add product images folder

--- script/procucts.js
+++ script/procucts.js
@@ -1,64 +1,66 @@
+const imgPath = 'assets/products/'
+
 export const products = [
     {
         title: 'iPhone 11 Apple (128GB) (PRODUCT)RED tela 6,1" Câmera 12MP iOS',
         price: "4096.80",
-        img: "https://images-americanas.b2w.io/produtos/01/00/img/1611344/8/1611344831_1SZ.jpg",
+        img: `${imgPath}/iphone.webp`,
         id: "iPhone_11",
         alt: "iPhone 11"
     },
     {
         title: 'Smart TV LED 32" Philco PTV32G52S HD e Áudio Dolby Conversor Digital',
         price: "1329.99",
-        img: "https://images-americanas.b2w.io/produtos/01/00/img/1322119/4/1322119477_1SZ.jpg",
+        img: `${imgPath}/tv.webp`,
         id: "Smart_TV",
         alt: "Smart TV"
     },
     {
         title: "Panela De Pressão Panelux Fechamento Externo 4.5L Alumínio",
         price: "109.99",
-        img: "https://images-americanas.b2w.io/produtos/01/00/img/134281/8/134281848_1SZ.jpg",
+        img: `${imgPath}/pan.webp`,
         id: "Panela_de_pressao",
         alt: "Panela de Pressão"
     },
     {
         title: 'Monitor LED 24" Samsung LC24f390 1920x1080 Full HD Curvo - Preto',
         price: "975.57",
-        img: "https://images-americanas.b2w.io/produtos/01/00/img7/01/00/item/133321/7/133321705SZ.jpg",
+        img: `${imgPath}/monitor.webp`,
         id: "Monitor",
         alt: "Monitor"
     },
     {
         title: "Mouse Gamer Logitech G203 LIGHTSYNC RGB Efeito de Ondas de Cores",
         price: "126.95",
-        img: "https://images-americanas.b2w.io/produtos/01/00/img/1720328/9/1720328928_1SZ.jpg",
+        img: `${imgPath}/mouse.webp`,
         id: "Mouse_Gamer",
         alt: "Mouse Gamer"
     },
     {
         title: "Cadeira Gamer Elite Preto - Dazz",
         price: "799.99",
-        img: "https://images-americanas.b2w.io/produtos/01/00/img/133776/0/133776048_1SZ.jpg",
+        img: `${imgPath}/chair.webp`,
         id: "Cadeira_Gamer",
         alt: "Cadeira Gamer"
     },
     {
         title: "Mesa Digitalizadora CTL472 One By Wacom Redwood",
         price: "287.09",
-        img: "https://images-americanas.b2w.io/produtos/01/00/img/133710/3/133710372_1SZ.jpg",
+        img: `${imgPath}/graphics-tablet.webp`,
         id: "Mesa_Digitalizadora",
         alt: "Mesa Digitalizadora"
     },
     {
         title: "Caneta Esferográfica Bic Fashion Desliza 10 Cores 930813 26600",
         price: "19.99",
-        img: "https://images-americanas.b2w.io/produtos/50862318/imagens/caneta-esferografica-bic-fashion-desliza-10-cores-930813-26600/50862318_1_xlarge.jpg",
+        img: `${imgPath}/pens.webp`,
         id: "Caneta_Esferografica",
         alt: "Caneta Esferográfica"
     },
     {
         title: "Fone de Ouvido Philips TAH1205 Bluetooth com Microfone",
         price: "129.99",
-        img: "https://images-americanas.b2w.io/produtos/01/00/img/2933926/7/2933926745_1SZ.jpg",
+        img: `${imgPath}/headphone.webp`,
         id: "Fone_de_Ouvido",
         alt: "Fone de ouvido"
     }
